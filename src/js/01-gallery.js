d2bd40c8d2664ba7426e0f749d25a6701a5576c3
shevalie30 <103785982+shevalie30@users.noreply.github.com>
2022-09-04 17:17:22 +0300

    ......

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,48 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const ref = {
+    containerForGallery: document.querySelector('.gallery'),
+}
 
+let galleryItem = '';
+const buildGallery = galleryItems.map((key) => {
+    galleryItem += `
+        <div class="gallery__item">
+        <a class="gallery__link" href="#">
+                <img
+                    class="gallery__image"
+                    src="${key.preview}"
+                    data-source="${key.original}"
+                    alt="${key.description}"
+                />
+                </a>
+        </div>`;
+})
+
+ref.containerForGallery.insertAdjacentHTML("afterbegin", galleryItem)
+
+ref.containerForGallery.addEventListener(`click`, openModalWindow)
+
+function openModalWindow(event) {
+    console.log(event.target.dataset.source);
+    event.preventDefault();
+    const instance = basicLightbox.create(`
+    <div class="modal">
+    <img src="${event.target.dataset.source}" width="800" height="600">
+    </div>
+	`);
+    instance.show();
+    window.addEventListener('keydown', function (e) {
+
+        if (!basicLightbox.visible()) { return };
+        if (e.key == 'Escape' || e.key == 'Esc' || e.key == 27) {
+            e.preventDefault();
+            instance.close();
+        }
+    });
+
+}
 console.log(galleryItems);
