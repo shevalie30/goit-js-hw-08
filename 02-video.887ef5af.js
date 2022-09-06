fa848f37918e77b574cc739c7cdbacb5d0079ac8
shevalie30 <103785982+shevalie30@users.noreply.github.com>
2022-09-06 14:49:29 +0000

    Deploying to gh-pages from @ shevalie30/goit-js-hw-08@bc5257d0afb980787481325c99bb5d4a6a3f0847 🚀

--- 02-video.887ef5af.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("#vimeo-player"),t=new Player(e);t.on("timeupdate",throttle((function(e){let t=e.seconds;console.log(t),localStorage.setItem("videoplayer-current-time",t)}),1e3));const o=localStorage.getItem("videoplayer-current-time");t.setCurrentTime(o).then((function(e){})).catch((function(e){e.name}));
-//# sourceMappingURL=02-video.887ef5af.js.map
