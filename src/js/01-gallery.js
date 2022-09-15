44561137abfdd9a74ce1b7f86cc51947c15a2130
shevalie30 <103785982+shevalie30@users.noreply.github.com>
2022-09-15 23:27:36 +0300

    ......

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,8 +1,7 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-// Change code below this line
+
 const ref = {
     containerForGallery: document.querySelector('.gallery'),
 };
